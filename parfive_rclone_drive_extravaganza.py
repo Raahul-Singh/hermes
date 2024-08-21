bbec8f953cd467fb70fdb57783ef7eaa38820bb1
Raahul Singh <raahulsingh002@gmail.com>
2024-08-21 17:44:20 +0530

    structure works

--- parfive_rclone_drive_extravaganza.py
+++ parfive_rclone_drive_extravaganza.py
@@ -8,25 +8,27 @@ PATH = Path(__file__).parent
 
 import os
 import shutil
-import subprocess
 
 import pandas as pd
 from parfive import Downloader
 from rclone_python import rclone
 
 # Constants
-LOCAL_DOWNLOAD_DIR = "fits_temp_storage"
+LOCAL_DOWNLOAD_DIR = "frames"
 GDRIVE_REMOTE_NAME = "gdrive"  # This should match the name you configured in rclone
 GDRIVE_DESTINATION_DIR = (
-    "hermes/75k"  # Replace with your desired destination folder on Google Drive
+    "hermes/frames2"  # Replace with your desired destination folder on Google Drive
 )
-BATCH_SIZE = 100  # Number of files to process in each batch
+BATCH_SIZE = 500  # Number of files to process in each batch
+BANDS = ["u", "g", "r", "i", "z"]
 
 # Ensure the local download directory exists
-os.makedirs(LOCAL_DOWNLOAD_DIR, exist_ok=True)
+
+for x in BANDS:
+    os.makedirs(f"{LOCAL_DOWNLOAD_DIR}/{x}", exist_ok=True)
 
 
-def get_fits_downloader(urls, outdir, max_conn=100):
+def get_fits_downloader(urls, outdir, max_conn=500):
     """Create a parfive downloader for FITS files."""
     dl = Downloader(max_conn=max_conn)
     for url in urls:
@@ -34,70 +36,55 @@ def get_fits_downloader(urls, outdir, max_conn=100):
     return dl
 
 
-def download_files(urls):
+def download_files(urls, band):
     """Download files using the provided downloader."""
-    downloader = get_fits_downloader(urls, LOCAL_DOWNLOAD_DIR)
+    downloader = get_fits_downloader(urls, f"{LOCAL_DOWNLOAD_DIR}/{band}")
     files = downloader.download()
 
     # Check for any failed downloads
     if files.errors:
-        for error in files.errors:
-            print(f"Error downloading {error.url}: {error.exception}")
-
+        with open(f"errors_{band}.txt", "a") as f:
+            f.write("\n".join([f"{file.error} - {file.url}" for file in files.errors]))
     return files
 
 
-def run_hail_mary(u, g, r, i, z):
+def run_hail_mary(df, data_path):
     """Process the DataFrame in batches, download files, and upload them to Google Drive."""
 
     # Split the DataFrame into chunks
-    for start in range(
-        2310, len(u), BATCH_SIZE
-    ):  # Start at 900 to avoid re-downloading
-        u_urls = u[start : start + BATCH_SIZE]
-        g_urls = g[start : start + BATCH_SIZE]
-        r_urls = r[start : start + BATCH_SIZE]
-        i_urls = i[start : start + BATCH_SIZE]
-        z_urls = z[start : start + BATCH_SIZE]
-        file_urls = u_urls + g_urls + r_urls + i_urls + z_urls
+    for start in range(0, len(df), BATCH_SIZE):  # Start at 900 to avoid re-downloading
+        # breakpoint()
+        temp_df = df.iloc[start : start + BATCH_SIZE]
+        filenames = temp_df["filenames"]
+        fits_url = temp_df["fits_url"]
+        for x in BANDS:
+            urls = fits_url.str.replace("frame-x-", f"frame-{x}-").tolist()
+            # Download files
+            download_files(urls, x)
 
-        # Download files
-        download_files(file_urls)
+            rclone.copy(
+                f"./{LOCAL_DOWNLOAD_DIR}/{x}",
+                f"gdrive:hermes/frames2/{x}/",
+                ignore_existing=True,
+                args=["-P", "--transfers=100", "--checkers=100"],
+            )
 
-        # Upload each file to Google Drive
-        subprocess.run(
-            ["rclone", "mkdir", f"gdrive:hermes/batch_post_900/batch_{start}/"]
-        )
-        rclone.copy(
-            f"./{LOCAL_DOWNLOAD_DIR}/",
-            f"gdrive:hermes/batch_post_900/batch_{start}/",
-            ignore_existing=True,
-            args=["-P", "--transfers=100"],
-        )
+            shutil.rmtree(f"{LOCAL_DOWNLOAD_DIR}/{x}", ignore_errors=True)
+            os.makedirs(f"{LOCAL_DOWNLOAD_DIR}/{x}", exist_ok=True)
 
-        shutil.rmtree(f"{LOCAL_DOWNLOAD_DIR}/", ignore_errors=True)
+        df.loc[df["filenames"].isin(filenames), "file_downloaded"] = 1
+        df.to_csv(data_path, index=False)
         print(f"Batch {start} complete")
-        os.makedirs(LOCAL_DOWNLOAD_DIR, exist_ok=True)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download Upload fits files")
-    parser.add_argument(
-        "--data_path", type=str, default="data_lt_1_sampled_75_k_rows_updated.csv"
-    )
+    parser.add_argument("--data_path", type=str, default="first75k_dataset.csv")
     parser.add_argument("--max_conn", type=int, default=100)
     args = parser.parse_args()
 
     df = pd.read_csv(args.data_path)
-    df = df.drop_duplicates(
-        subset=["fits_url_z"]
-    )  # Remove duplicates for u, g, r, i, z All same
-    urls = []
-
-    run_hail_mary(
-        df["fits_url_u"].tolist(),
-        df["fits_url_g"].tolist(),
-        df["fits_url_r"].tolist(),
-        df["fits_url_i"].tolist(),
-        df["fits_url_z"].tolist(),
-    )
+    df = df[
+        df["file_downloaded"] == 0
+    ]  # Only process files that haven't been downloaded yet
+    run_hail_mary(df, data_path=args.data_path)
